8eb6390f3523b8a324c39107e59503d9cd30ebb0
rakibulhasan044 <rhrakib044@gmail.com>
2025-06-12 20:03:21 +0600

    added next shop

---
+++ next-shop/src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,11 @@
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to NextMart home page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ next-shop/src/app/(withDashboardLayout)/layout.tsx
@@ -0,0 +1,6 @@
+
+const DashboardPage = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default DashboardPage;
