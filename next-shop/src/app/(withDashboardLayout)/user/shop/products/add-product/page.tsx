82b18c5ec4b03974cd2bf55a627326c5234037b5
rakibulhasan044 <rhrakib044@gmail.com>
2025-07-04 03:12:42 +0600

    add product form

---
+++ next-shop/src/app/(withDashboardLayout)/user/shop/products/add-product/page.tsx
@@ -0,0 +1,11 @@
+import AddProductsForm from "@/components/modules/shop/products/AddProductForm";
+
+const AddProductsPage = () => {
+    return (
+        <div>
+            <AddProductsForm />
+        </div>
+    );
+};
+
+export default AddProductsPage;
