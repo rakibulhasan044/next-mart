fec2c05911ed2994a8b80083476637bafabf70c3
rakibulhasan044 <rhrakib044@gmail.com>
2025-07-09 03:56:30 +0600

    add pr oducts to the cart

--- next-shop/src/redux/store.ts
+++ next-shop/src/redux/store.ts
@@ -1,13 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./features/cartSlice";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
-  })
-}
+    reducer: {
+      cart: cartSlice,
+    },
+  });
+};
 
 // Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>
+export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
