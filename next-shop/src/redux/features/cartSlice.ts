90bce15454c7c2500aee588413ebd450c05c3a40
rakibulhasan044 <rhrakib044@gmail.com>
2025-07-09 04:14:53 +0600

    Managing Multiple Products

--- next-shop/src/redux/features/cartSlice.ts
+++ next-shop/src/redux/features/cartSlice.ts
@@ -1,10 +1,13 @@
-
 import { IProduct } from "@/types";
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
+export interface CartProduct extends IProduct {
+  orderQuantity: number;
+}
+
 interface InitialState {
-  products: IProduct[];
+  products: CartProduct[];
 }
 
 const initialState: InitialState = {
@@ -16,13 +19,22 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addProduct: (state, action) => {
-        state.products.push(action.payload)
-    }
+      const productToAdd = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+
+      if (productToAdd) {
+        productToAdd.orderQuantity += 1;
+        return;
+      }
+
+      state.products.push({ ...action.payload, orderQuantity: 1 });
+    },
   },
 });
 
-export const orderedProductsSelector =(state: RootState) => {
-    return state.cart.products
-}
-export const { addProduct } = cartSlice.actions
+export const orderedProductsSelector = (state: RootState) => {
+  return state.cart.products;
+};
+export const { addProduct } = cartSlice.actions;
 export default cartSlice.reducer;
