fddd6ea7e2ff1b457a173b1332b57d815aa8f9ad
rakibulhasan044 <rhrakib044@gmail.com>
2025-07-10 00:26:47 +0600

    Cart Validation

--- next-shop/src/components/modules/cart/PaymentDetails.tsx
+++ next-shop/src/components/modules/cart/PaymentDetails.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import {  orderSelector, shippingCostSelector, subTotalSelector } from "@/redux/features/cartSlice";
+import { currencyFormatter } from "@/lib/currencyFormatter";
+import {  citySelector, grandTotalSelector, orderedProductsSelector, orderSelector, shippingAddressSelector, shippingCostSelector, subTotalSelector } from "@/redux/features/cartSlice";
 import { useAppSelector } from "@/redux/hooks";
+import { toast } from "sonner";
 
 
 export default function PaymentDetails() {
@@ -9,9 +11,27 @@ export default function PaymentDetails() {
   const subTotal = useAppSelector(subTotalSelector).toFixed(2)
   const shippingCost = useAppSelector(shippingCostSelector)
   const order = useAppSelector(orderSelector);
+  const grandTotal = useAppSelector(grandTotalSelector)
+  const city = useAppSelector(citySelector)
+  const shippingAddress = useAppSelector(shippingAddressSelector);
+  const cartProducts = useAppSelector(orderedProductsSelector);
 
   const handleOrder = () => {
-    console.log(order);
+    const orderLoading = toast.loading("Order is being placed")
+    try {
+      if(!city) {
+        throw new Error("City is missing")
+      }
+      if(!shippingAddress) {
+        throw new Error("Shipping address is missing")
+      }
+      if(cartProducts.length === 0) {
+        throw new Error("Cart is empty")
+      }
+      toast.success("Order placed successfully", {id: orderLoading})
+    } catch (error: any) {
+      toast.error(error.message, {id: orderLoading})
+    }
   }
 
   return (
@@ -33,7 +53,7 @@ export default function PaymentDetails() {
       </div>
       <div className="flex justify-between mt-10 mb-5">
         <p className="text-gray-500 ">Grand Total</p>
-        <p className="font-semibold">pp</p>
+        <p className="font-semibold">{currencyFormatter(grandTotal)}</p>
       </div>
       <Button
         onClick={handleOrder}
